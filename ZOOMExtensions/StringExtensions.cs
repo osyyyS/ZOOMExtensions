5776b2978275b244f78778abc9d0213fc90f07f6
osyyyS <78726017+osyyyS@users.noreply.github.com>
2023-08-09 14:31:03 +0200

    Add IsValidEmail

---
+++ ZOOMExtensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ZOOMExtensions
+{
+    public static partial class StringExtensions
+    {
+        [GeneratedRegex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")]
+        private static partial Regex EmailRegex();
+
+        public static bool IsValidEmail(this string email)
+        {
+            if (string.IsNullOrEmpty(email))
+                return false;
+            var regex = EmailRegex();
+            return regex.IsMatch(email);
+        }
+    }
+}
