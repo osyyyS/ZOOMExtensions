066b799fb83eb31eb04e04f778c12c835fe619be
Oscar <78726017+osyyyS@users.noreply.github.com>
2023-08-15 10:14:06 +0200

    Add GetMainModuleNameSafe + Tests

---
+++ ZOOMExtensions/ProcessExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZOOMExtensions
+{
+    public static class ProcessExtensions
+    {
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(uint processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("psapi.dll")]
+        private static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In][MarshalAs(UnmanagedType.U4)] int nSize);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string? GetMainModuleNameSafe(this Process process)
+        {
+            var processHandle = OpenProcess(0x0400 | 0x0010, false, process.Id);
+
+            if (processHandle == IntPtr.Zero)
+            {
+                return null;
+            }
+
+            const int lengthSb = 4000;
+
+            var sb = new StringBuilder(lengthSb);
+
+            string? result = null;
+
+            if (GetModuleFileNameEx(processHandle, IntPtr.Zero, sb, lengthSb) > 0)
+            {
+                result = Path.GetFileName(sb.ToString());
+            }
+
+            CloseHandle(processHandle);
+
+            return result;
+        }
+    }
+}
